67e7400333e78fb956eacd78406ab911956a26fe
Sergey Shulyak <sergey.shulyak8@gmail.com>
2019-06-13 19:29:56 +0300

    Started setting up microservices

--- packages/api-gateway/src/app.module.ts
+++ packages/api-gateway/src/app.module.ts
@@ -1,8 +1,7 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [],
 })
 export class AppModule {}
--- packages/user-service/src/main.ts
+++ packages/user-service/src/main.ts
@@ -1,8 +1,11 @@
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
+import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.RMQ,
+  });
+  app.listen(() => console.log('User Service is listening'))
 }
-bootstrap()
+bootstrap();
